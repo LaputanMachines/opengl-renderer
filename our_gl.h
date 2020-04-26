683aca8b7bd2362a622499b1e7b5cca6e752156d
Michael Bassili <mbassili@sfu.ca>
2020-04-26 13:51:11 -0700

    Populate 2,3 of viewport matrix with 1s, use look-at call to project onto model

--- our_gl.h
+++ our_gl.h
@@ -4,9 +4,8 @@
 #include "geometry.h"
 
 extern Matrix ModelView;
-extern Matrix Viewport;
 extern Matrix Projection;
-const float depth = 2000.f;
+extern Matrix Viewport;
 
 void viewport(int x, int y, int w, int h);
 void projection(float coeff=0.f); // coeff = -1/c
@@ -15,9 +14,10 @@ void lookat(Vec3f eye, Vec3f center, Vec3f up);
 struct IShader {
     virtual ~IShader();
     virtual Vec4f vertex(int iface, int nthvert) = 0;
-    virtual bool fragment(Vec3f bar, TGAColor &color) = 0;
+    virtual bool fragment(Vec3f gl_FragCoord, Vec3f bar, TGAColor &color) = 0;
 };
 
-void triangle(Vec4f *pts, IShader &shader, TGAImage &image, float *zbuffer);
+//void triangle(Vec4f *pts, IShader &shader, TGAImage &image, float *zbuffer);
+void triangle(mat<4,3,float> &pts, IShader &shader, TGAImage &image, float *zbuffer);
 #endif //__OUR_GL_H__
 
