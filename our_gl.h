d4a83c7ba10eccb6e99979e34ca4328a08934d09
Michael Bassili <mbassili@sfu.ca>
2020-04-26 12:54:24 -0700

    Clamp image to 2D buffer to prevent overflow

--- our_gl.h
+++ our_gl.h
@@ -1,11 +1,9 @@
 #ifndef __OUR_GL_H__
 #define __OUR_GL_H__
-
 #include "tgaimage.h"
 #include "geometry.h"
 
 extern Matrix ModelView;
-extern Matrix Viewport;
 extern Matrix Projection;
 
 void viewport(int x, int y, int w, int h);
@@ -18,7 +16,7 @@ struct IShader {
     virtual bool fragment(Vec3f bar, TGAColor &color) = 0;
 };
 
-void triangle(Vec4f *pts, IShader &shader, TGAImage &image, TGAImage &zbuffer);
-
+//void triangle(Vec4f *pts, IShader &shader, TGAImage &image, float *zbuffer);
+void triangle(mat<4,3,float> &pts, IShader &shader, TGAImage &image, float *zbuffer);
 #endif //__OUR_GL_H__
 
