f9a7fe2d29fbfd8e6b3a01ea2552b9fec86b0dc5
Michael Bassili <mbassili@sfu.ca>
2020-04-26 13:36:59 -0700

    Update model view buffers for all x, y, z, axis

--- our_gl.h
+++ our_gl.h
@@ -4,7 +4,9 @@
 #include "geometry.h"
 
 extern Matrix ModelView;
+extern Matrix Viewport;
 extern Matrix Projection;
+const float depth = 2000.f;
 
 void viewport(int x, int y, int w, int h);
 void projection(float coeff=0.f); // coeff = -1/c
@@ -16,7 +18,6 @@ struct IShader {
     virtual bool fragment(Vec3f bar, TGAColor &color) = 0;
 };
 
-//void triangle(Vec4f *pts, IShader &shader, TGAImage &image, float *zbuffer);
-void triangle(mat<4,3,float> &pts, IShader &shader, TGAImage &image, float *zbuffer);
+void triangle(Vec4f *pts, IShader &shader, TGAImage &image, float *zbuffer);
 #endif //__OUR_GL_H__
 
