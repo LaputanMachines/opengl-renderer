345b55a27148cf014883f227b34218bfaad45def
Michael Bassili <mbassili@sfu.ca>
2020-04-26 11:56:31 -0700

    Add handling for three dimensional indexing of pixels

--- model.cpp
+++ model.cpp
@@ -17,14 +17,14 @@ Model::Model(const char *filename) : verts_(), faces_() {
         if (!line.compare(0, 2, "v ")) {
             iss >> trash;
             Vec3f v;
-            for (int i=0;i<3;i++) iss >> v.raw[i];
+            for (int i=0;i<3;i++) iss >> v[i];
             verts_.push_back(v);
         } else if (!line.compare(0, 2, "f ")) {
             std::vector<int> f;
             int itrash, idx;
             iss >> trash;
             while (iss >> idx >> trash >> itrash >> trash >> itrash) {
-                idx--;
+                idx--; // in wavefront obj all indices start at 1, not zero
                 f.push_back(idx);
             }
             faces_.push_back(f);
