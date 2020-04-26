354fc312928405fcf37592efba9a265b5e9bc2b3
Michael Bassili <mbassili@sfu.ca>
2020-04-26 12:46:19 -0700

    General refactor, apply Gouraud Shader to verticies

--- main.cpp
+++ main.cpp
@@ -1,77 +1,36 @@
 #include <vector>
 #include <iostream>
-#include <cmath>
-#include <limits>
+
 #include "tgaimage.h"
 #include "model.h"
 #include "geometry.h"
+#include "our_gl.h"
 
+Model *model     = NULL;
 const int width  = 800;
 const int height = 800;
-const int depth  = 255;
 
-Model *model = NULL;
-int *zbuffer = NULL;
-Vec3f light_dir = Vec3f(1,-1,1).normalize();
-Vec3f eye(1,1,3);
-Vec3f center(0,0,0);
+Vec3f light_dir(1,1,1);
+Vec3f       eye(0,-1,3);
+Vec3f    center(0,0,0);
+Vec3f        up(0,1,0);
 
-Matrix viewport(int x, int y, int w, int h) {
-    Matrix m = Matrix::identity(4);
-    m[0][3] = x+w/2.f;
-    m[1][3] = y+h/2.f;
-    m[2][3] = depth/2.f;
+struct GouraudShader : public IShader {
+    Vec3f varying_intensity; // written by vertex shader, read by fragment shader
 
-    m[0][0] = w/2.f;
-    m[1][1] = h/2.f;
-    m[2][2] = depth/2.f;
-    return m;
-}
-
-Matrix lookat(Vec3f eye, Vec3f center, Vec3f up) {
-    Vec3f z = (eye-center).normalize();
-    Vec3f x = (up^z).normalize();
-    Vec3f y = (z^x).normalize();
-    Matrix res = Matrix::identity(4);
-    for (int i=0; i<3; i++) {
-        res[0][i] = x[i];
-        res[1][i] = y[i];
-        res[2][i] = z[i];
-        res[i][3] = -center[i];
+    virtual Vec4f vertex(int iface, int nthvert) {
+        Vec4f gl_Vertex = embed<4>(model->vert(iface, nthvert)); // read the vertex from .obj file
+        gl_Vertex = Viewport*Projection*ModelView*gl_Vertex;     // transform it to screen coordinates
+        varying_intensity[nthvert] = std::max(0.f, model->normal(iface, nthvert)*light_dir); // get diffuse lighting intensity
+        return gl_Vertex;
     }
-    return res;
-}
 
-void triangle(Vec3i t0, Vec3i t1, Vec3i t2, float ity0, float ity1, float ity2, TGAImage &image, int *zbuffer) {
-    if (t0.y==t1.y && t0.y==t2.y) return; // i dont care about degenerate triangles
-    if (t0.y>t1.y) { std::swap(t0, t1); std::swap(ity0, ity1); }
-    if (t0.y>t2.y) { std::swap(t0, t2); std::swap(ity0, ity2); }
-    if (t1.y>t2.y) { std::swap(t1, t2); std::swap(ity1, ity2); }
-
-    int total_height = t2.y-t0.y;
-    for (int i=0; i<total_height; i++) {
-        bool second_half = i>t1.y-t0.y || t1.y==t0.y;
-        int segment_height = second_half ? t2.y-t1.y : t1.y-t0.y;
-        float alpha = (float)i/total_height;
-        float beta  = (float)(i-(second_half ? t1.y-t0.y : 0))/segment_height; // be careful: with above conditions no division by zero here
-        Vec3i A    =               t0  + Vec3f(t2-t0  )*alpha;
-        Vec3i B    = second_half ? t1  + Vec3f(t2-t1  )*beta : t0  + Vec3f(t1-t0  )*beta;
-        float ityA =               ity0 +   (ity2-ity0)*alpha;
-        float ityB = second_half ? ity1 +   (ity2-ity1)*beta : ity0 +   (ity1-ity0)*beta;
-        if (A.x>B.x) { std::swap(A, B); std::swap(ityA, ityB); }
-        for (int j=A.x; j<=B.x; j++) {
-            float phi = B.x==A.x ? 1. : (float)(j-A.x)/(B.x-A.x);
-            Vec3i    P = Vec3f(A) +  Vec3f(B-A)*phi;
-            float ityP =    ityA  + (ityB-ityA)*phi;
-            int idx = P.x+P.y*width;
-            if (P.x>=width||P.y>=height||P.x<0||P.y<0) continue;
-            if (zbuffer[idx]<P.z) {
-                zbuffer[idx] = P.z;
-                image.set(P.x, P.y, TGAColor(255, 255, 255)*ityP);
-            }
-        }
+    virtual bool fragment(Vec3f bar, TGAColor &color) {
+        float intensity = varying_intensity*bar;   // interpolate intensity for the current pixel
+        color = TGAColor(255, 255, 255)*intensity; // well duh
+        return false;                              // no, we do not discard this pixel
     }
-}
+};
 
 int main(int argc, char** argv) {
     if (2==argc) {
@@ -80,53 +39,28 @@ int main(int argc, char** argv) {
         model = new Model("obj/african_head.obj");
     }
 
-    zbuffer = new int[width*height];
-    for (int i=0; i<width*height; i++) {
-        zbuffer[i] = std::numeric_limits<int>::min();
-    }
+    lookat(eye, center, up);
+    viewport(width/8, height/8, width*3/4, height*3/4);
+    projection(-1.f/(eye-center).norm());
+    light_dir.normalize();
 
-    { // draw the model
-        Matrix ModelView  = lookat(eye, center, Vec3f(0,1,0));
-        Matrix Projection = Matrix::identity(4);
-        Matrix ViewPort   = viewport(width/8, height/8, width*3/4, height*3/4);
-        Projection[3][2] = -1.f/(eye-center).norm();
+    TGAImage image  (width, height, TGAImage::RGB);
+    TGAImage zbuffer(width, height, TGAImage::GRAYSCALE);
 
-        std::cerr << ModelView << std::endl;
-        std::cerr << Projection << std::endl;
-        std::cerr << ViewPort << std::endl;
-        Matrix z = (ViewPort*Projection*ModelView);
-        std::cerr << z << std::endl;
-
-        TGAImage image(width, height, TGAImage::RGB);
-        for (int i=0; i<model->nfaces(); i++) {
-            std::vector<int> face = model->face(i);
-            Vec3i screen_coords[3];
-            Vec3f world_coords[3];
-            float intensity[3];
-            for (int j=0; j<3; j++) {
-                Vec3f v = model->vert(face[j]);
-                screen_coords[j] =  Vec3f(ViewPort*Projection*ModelView*Matrix(v));
-                world_coords[j]  = v;
-                intensity[j] = model->norm(i, j)*light_dir;
-            }
-            triangle(screen_coords[0], screen_coords[1], screen_coords[2], intensity[0], intensity[1], intensity[2], image, zbuffer);
+    GouraudShader shader;
+    for (int i=0; i<model->nfaces(); i++) {
+        Vec4f screen_coords[3];
+        for (int j=0; j<3; j++) {
+            screen_coords[j] = shader.vertex(i, j);
         }
-        image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
-        image.write_tga_file("output.tga");
+        triangle(screen_coords, shader, image, zbuffer);
     }
 
-    { // dump z-buffer (debugging purposes only)
-        TGAImage zbimage(width, height, TGAImage::GRAYSCALE);
-        for (int i=0; i<width; i++) {
-            for (int j=0; j<height; j++) {
-                zbimage.set(i, j, TGAColor(zbuffer[i+j*width]));
-            }
-        }
-        zbimage.flip_vertically(); // i want to have the origin at the left bottom corner of the image
-        zbimage.write_tga_file("zbuffer.tga");
-    }
+    image.  flip_vertically(); // to place the origin in the bottom left corner of the image
+    zbuffer.flip_vertically();
+    image.  write_tga_file("output.tga");
+    zbuffer.write_tga_file("zbuffer.tga");
+
     delete model;
-    delete [] zbuffer;
     return 0;
 }
-
