d695fcb133c5b2fed19556d8b03ca49676d4b6f5
Michael Bassili <mbassili@sfu.ca>
2020-04-26 12:24:03 -0700

    Add norm func to model class

--- model.cpp
+++ model.cpp
@@ -5,7 +5,7 @@
 #include <vector>
 #include "model.h"
 
-Model::Model(const char *filename) : verts_(), faces_(), norms_(), uv_() {
+Model::Model(const char *filename) : verts_(), faces_(), norms_(), uv_(), diffusemap_() {
     std::ifstream in;
     in.open (filename, std::ifstream::in);
     if (in.fail()) return;
@@ -82,6 +82,11 @@ TGAColor Model::diffuse(Vec2i uv) {
 Vec2i Model::uv(int iface, int nvert) {
     int idx = faces_[iface][nvert][1];
     return Vec2i(uv_[idx].x*diffusemap_.get_width(), uv_[idx].y*diffusemap_.get_height());
+}
+
+Vec3f Model::norm(int iface, int nvert) {
+    int idx = faces_[iface][nvert][2];
+    return norms_[idx].normalize();
 }
 
 
